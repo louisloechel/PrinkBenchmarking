5749beb75e42307424aa64228fa5f3dfecdd20d4
louisloechel <57091589+louisloechel@users.noreply.github.com>
2024-08-02 14:25:36 +0200

    add first tf files (wip)

---
+++ tf/main.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region = var.aws_region
+
+}
+
+# TODO: to be completed. check aws specific documentation
---
+++ tf/variables.tf
@@ -0,0 +1,6 @@
+variable "aws_region" {
+  description = "The AWS region to deploy to"
+  default     = "eu-west-1"
+}
+
+# TODO: to be completed. check aws specific documentation
