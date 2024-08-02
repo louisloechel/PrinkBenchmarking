c20836a0b9c6ed970676a22e9d5833bb8a57c8d9
louisloechel <57091589+louisloechel@users.noreply.github.com>
2024-08-02 13:19:45 +0200

    minimum working state,  output to console

--- client/main.go
+++ client/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"log"
+	"net/url"
 	"os"
 	"time"
 
@@ -17,7 +18,6 @@ type Metric struct {
 type Config struct {
 	Address      string `yaml:"sut_address"`
 	Port         int    `yaml:"sut_port"`
-	Concurrency  int    `yaml:"concurrency"`
 	Mode         string `yaml:"mode"`
 	Interval     int    `yaml:"interval"`
 	Duration     int    `yaml:"duration"`
@@ -49,8 +49,13 @@ func loadConfig(path string) Config {
 }
 
 func initialiseResults(path string) {
+	// Create the directory if it doesn't exist
+	if err := os.MkdirAll(path, os.ModePerm); err != nil {
+		log.Fatalf("Could not create output directory: %v", err)
+	}
+
 	// open file for appending, create if it doesn't exist
-	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(path+"/results.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatalf("Could not open results file: %v", err)
 	}
@@ -104,35 +109,100 @@ func loadDataset(path string) [][]string {
 	return dataset
 }
 
-func warmUp() {
+func warmUp(dataset [][]string, duration int, u url.URL) {
+	// warm up the SUT
+	// Iterate over the records for duration seconds and write them to Kafka
+	count := 0
+	start := time.Now()
+	for _, record := range dataset {
+		t_s := time.Now()
 
+		// Message fields:
+		// building_id, timestamp, meter_reading, primary_use, square_feet, year_built, floor_count, air_temperature, cloud_coverage, dew_temperature, precip_depth_1_hr, sea_level_pressure, wind_direction, wind_speed
+
+		message := fmt.Sprintf("t_s: %v, message_id: %d, building_id: %s, timestamp: %s, meter_reading: %s, primary_use: %s, square_feet: %s, year_built: %s, floor_count: %s, air_temperature: %s, cloud_coverage: %s, dew_temperature: %s, precip_depth_1_hr: %s, sea_level_pressure: %s, wind_direction: %s, wind_speed: %s",
+			t_s, count, record[0], record[1], record[2], record[3], record[4], record[5], record[6], record[7], record[8], record[9], record[10], record[11], record[12], record[13])
+
+		// TODO: Write the message to Flink
+
+		if count%1000000 == 0 {
+			log.Printf("Wrote record: %s to %s", message, u.String())
+		}
+		// Check if the duration has passed
+		if time.Since(start) > time.Duration(duration)*time.Second {
+			break
+		}
+
+		count++
+	}
+	passedTime := time.Since(start)
+	log.Printf("Wrote all %d records to Flink in %s before warmup time of %ds ended.", len(dataset), passedTime.String(), duration)
 }
 
-func benchmark() {
+func benchmark(dataset [][]string, duration int, u url.URL) {
+	// benchmark the SUT
+	// Iterate over the records for duration seconds and write them to Kafka
+	count := 0
+	start := time.Now()
+	for _, record := range dataset {
+		t_s := time.Now()
 
+		// Message fields:
+		// building_id, timestamp, meter_reading, primary_use, square_feet, year_built, floor_count, air_temperature, cloud_coverage, dew_temperature, precip_depth_1_hr, sea_level_pressure, wind_direction, wind_speed
+
+		message := fmt.Sprintf("t_s: %v, message_id: %d, building_id: %s, timestamp: %s, meter_reading: %s, primary_use: %s, square_feet: %s, year_built: %s, floor_count: %s, air_temperature: %s, cloud_coverage: %s, dew_temperature: %s, precip_depth_1_hr: %s, sea_level_pressure: %s, wind_direction: %s, wind_speed: %s",
+			t_s, count, record[0], record[1], record[2], record[3], record[4], record[5], record[6], record[7], record[8], record[9], record[10], record[11], record[12], record[13])
+
+		if count%1000000 == 0 {
+			log.Printf("Wrote record: %s to %s", message, u.String())
+		}
+
+		// Check if the duration has passed
+		if time.Since(start) > time.Duration(duration)*time.Second {
+			break
+		}
+
+		count++
+	}
+
+	passedTime := time.Since(start)
+	log.Printf("Wrote all %d records to Flink in %s before Benchmark time of %ds ended.", len(dataset), passedTime.String(), duration)
 }
 
 func experimentDone() {
-
+	// Create experiment_done.txt
+	file, err := os.Create("../results/experiment_done.txt")
+	if err != nil {
+		log.Fatalf("Could not create experiment_done.txt: %v", err)
+	}
+	defer file.Close()
 }
 
 func main() {
-	config := loadConfig("/client/config.yml")
+	config := loadConfig("config.yml")
 
-	// endpoint := fmt.Sprintf("%s:%d", config.Address, config.Port)
+	// parse url
+	u, err := url.Parse("https://" + config.Address + ":" + fmt.Sprintf("%d", config.Port))
+	if err != nil {
+		log.Fatalf("Could not parse URL: %v", err)
+	}
 
-	resultFile := config.OutputFolder + "/results.csv"
-	initialiseResults(resultFile)
+	// TODO?: set up connection to Flink
 
-	loadDataset()
+	initialiseResults(config.OutputFolder)
+
+	dataset := loadDataset(config.InputData)
 
 	log.Printf("Warming up the SUT for %d Seconds", config.Warmup)
-	warmUp()
+	warmUp(dataset, config.Warmup, *u)
 	log.Printf("Warmup done.")
 
 	log.Printf("Starting the benchmark for %d Seconds", config.Duration)
-	benchmark()
+	benchmark(dataset, config.Duration, *u)
+	log.Printf("Benchmark done.")
+
+	// TODO: Close connection
 
 	experimentDone()
-	log.Printf("Experiment done. Created output file: %s", resultFile)
+	log.Printf("Created output file in: %s", config.OutputFolder)
 }
---
+++ go.mod
@@ -0,0 +1,10 @@
+module prinkbenchmarking
+
+go 1.21.5
+
+require github.com/go-yaml/yaml v2.1.0+incompatible
+
+require (
+	gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
