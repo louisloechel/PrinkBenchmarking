c20278488cf294c3075cda106c317b708c856bb1
Thomas Janke <janke@tu-berlin.de>
2024-08-06 12:08:30 +0200

    Update benchmark loop to skip the first record

--- client/main.go
+++ client/main.go
@@ -130,7 +130,10 @@ func benchmark(dataset [][]string, conn net.Conn) {
 	// Iterate over the records for duration seconds and write them to console (for now)
 	count := 0
 	start := time.Now()
-	for _, record := range dataset {
+	for i, record := range dataset {
+		if i == 0 {
+			continue
+		}
 		t_s := time.Now()
 
 		// Bench fields:
@@ -139,8 +142,8 @@ func benchmark(dataset [][]string, conn net.Conn) {
 		// Data fields:
 		// building_id, timestamp, meter_reading, primary_use, square_feet, year_built, floor_count, air_temperature, cloud_coverage, dew_temperature, precip_depth_1_hr, sea_level_pressure, wind_direction, wind_speed
 
-		message := fmt.Sprintf("t_s: %v, message_id: %d, building_id: %s, timestamp: %s, meter_reading: %s, primary_use: %s, square_feet: %s, year_built: %s, floor_count: %s, air_temperature: %s, cloud_coverage: %s, dew_temperature: %s, precip_depth_1_hr: %s, sea_level_pressure: %s, wind_direction: %s, wind_speed: %s",
-			t_s, count, record[0], record[1], record[2], record[3], record[4], record[5], record[6], record[7], record[8], record[9], record[10], record[11], record[12], record[13])
+		message := fmt.Sprintf("%d,%v,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",
+			count, t_s, record[0], record[1], record[2], record[3], record[4], record[5], record[6], record[7], record[8], record[9], record[10], record[11], record[12], record[13])
 
 		// Write the message to Flink socket
 		_, err := conn.Write([]byte(message))
